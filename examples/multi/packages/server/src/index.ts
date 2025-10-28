f7b5c5bbb874187a9241eb9f144fadf6e81c5935
Tomohiko Ozawa <kota65535@gmail.com>
2025-10-28 22:43:13 +0900

    docs: add README

---
+++ examples/multi/packages/server/src/index.ts
@@ -0,0 +1,106 @@
+import type {ValidationTargets} from 'hono'
+import {Hono} from "hono";
+import {z, ZodSchema} from "zod";
+import {HTTPException} from "hono/http-exception";
+import {drizzle} from "drizzle-orm/node-postgres";
+import {greetingsTable} from "./db/schema";
+import {eq} from "drizzle-orm";
+import {zValidator as zv} from '@hono/zod-validator'
+
+const db = drizzle(process.env.DATABASE_URL!);
+
+const app = new Hono();
+
+const greetingSchema = z.object({
+	greeting: z.string().max(100, "Greeting must be 100 characters or less"),
+});
+const greetingParamSchema = z.object({
+	id: z.coerce.number().int().positive(),
+});
+const greetingQuerySchema = z.object({
+	offset: z.coerce.number().int().min(0).max(100).default(0),
+	limit: z.coerce.number().int().min(1).max(100).default(10),
+});
+
+export const zValidator = <T extends ZodSchema, Target extends keyof ValidationTargets>(
+	target: Target,
+	schema: T
+) =>
+	zv(target, schema, (result, c) => {
+		if (!result.success) {
+			const error = result.error.issues[0];
+			const res = c.json({ error: { message: error.message, path: error.path }});
+			throw new HTTPException(400, { cause: result.error, message: error.message, res })
+		}
+	})
+
+app.onError((error, c) => {
+	console.error(error);
+	if (error instanceof HTTPException) {
+		if (error.res) {
+			return error.getResponse();
+		} else {
+			return c.json({ error: { message: error.message }}, error.status);
+		}
+	}
+	return c.json({ error: { message: error.message }}, 500);
+});
+
+app
+    .get("/", zValidator("query", greetingQuerySchema), async (c) => {
+		const { offset, limit } = c.req.valid("query");
+		const items = await (async () => {
+			try {
+				return await db
+					.select()
+					.from(greetingsTable)
+					.offset(offset)
+					.limit(limit);
+			} catch (error) {
+				console.log(error);
+				throw new HTTPException(500, { cause: error });
+			}
+		})();
+		const totalCount = await (async () => {
+			try {
+				return await db.$count(greetingsTable);
+			} catch (error) {
+				throw new HTTPException(500, { cause: error });
+			}
+		})();
+		return c.json({ items, totalCount });
+	})
+	.get("/:id", zValidator("param", greetingParamSchema), async (c) => {
+		const { id } = c.req.valid("param");
+		const greetings = await (async () => {
+			try {
+				return await db
+					.select()
+					.from(greetingsTable)
+					.where(eq(greetingsTable.id, id))
+					.limit(1);
+			} catch (error) {
+				throw new HTTPException(500, { cause: error });
+			}
+		})();
+		if (greetings.length === 0) {
+			throw new HTTPException(404);
+		}
+		return c.json(greetings[0]);
+	})
+	.post("/", zValidator("json", greetingSchema), async (c) => {
+		const greeting = c.req.valid("json");
+		const inserted = await (async () => {
+			try {
+				return await db.insert(greetingsTable).values(greeting).returning();
+			} catch (error) {
+				throw new HTTPException(500, { cause: error });
+			}
+		})();
+		return c.json(inserted);
+	});
+
+export default {
+	port: process.env.PORT ?? 3000,
+	fetch: app.fetch,
+};
--- examples/single/README.md
+++ examples/single/README.md
@@ -1,9 +1,18 @@
 # Single project example
 
-Run `fire dev` to run server with dependencies.
+## Tech stack
 
-Created by:
+- Bun
+- Hono
 
-```
-bun create hono@latest single
-```
+Initialized by `bun create hono@latest single`.
+
+## Setup
+
+1. Install [mise](https://mise.jdx.dev/)
+2. Run `mise trust`
+3. Run `mise install`
+
+## Tasks
+
+Run `fire` to see the list of available tasks.
